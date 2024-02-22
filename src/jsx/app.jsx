bd53dfce3191fb1e4c8e3d726d40389ba6ab60f9
Crow613 <hsagoyan@gmail.com>
2024-02-22 09:27:25 -0500

    Registr page is ready

--- src/jsx/app.jsx
+++ src/jsx/app.jsx
@@ -1,14 +1,40 @@
 import '../css/app.css';
 
+const name = 'name'
+const email = 'email'
+const pass = 'password'
+const file = 'file'
 
-function App() {
+function App()
+{
 
-    return (
-          <>
-        <h1>Hi ;D</h1>
-        </>
-    );
+return (<>
+                
+                <dir className="div">
+             <form className='form' action="" method="post">
+                <p>Register</p>
+                
+             <button className='send2' type="submit">{`SignIn`}</button>
+              <input className='file' type={`${file}`} name="" id="" />
+              
+              <input className='name' type={ `${name}`} name="" id=""  placeholder='Name'/>
+              
+              <input className='email' type={ `${email}`} name="" id="" placeholder='Email'/>
+              
+              <input className='pass' type={`${pass}`} name="" id="" placeholder='New Password' />
+              <input className='repPass' type={`${pass}`} name="" id="" placeholder='Repeat Password' />
+              <button className='send'  type="submit">{`SignUp`}</button>
+              
+          
+              </form>
+
+              </dir>  
+                  
+     </>)
+                       
     
 }
 
 export default App
+
+   
--- src/jsx/main.jsx
+++ src/jsx/main.jsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client'
 import App from '../jsx/app'
 import '../css/index.css'
 
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App/>
