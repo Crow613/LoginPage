482d8cb45d4177fdd9a329eca7c9ddfdd50d7db4
Crow613 <hsagoyan@gmail.com>
2024-02-25 23:11:00 +0400

    changed style

--- src/jsx/app.jsx
+++ src/jsx/app.jsx
@@ -10,7 +10,18 @@ function App()
 
 return (<>
                 
-                <dir className="div">
+                <dir className="div">                 
+
+                <div className='fig'></div>
+                <div className='fig2'></div>
+                <div className='fig3'></div>
+                <div className='fig4'></div>
+                <div className='fig5'></div>              
+                <div className='fig6'></div>
+                <div className='fig7'></div>
+                <div className='fig8'></div>
+                <div className='fig9'></div>
+
              <form className='form' action="" method="post">
                 <p>Register</p>
                 
